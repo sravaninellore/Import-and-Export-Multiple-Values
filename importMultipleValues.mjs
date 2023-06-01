14610f94deed1d5cd628fd8321927f70818385b1
sravaninellore <nellorevenkatasravani@gmail.com>
2023-06-01 11:44:15 +0000

    commit message

---
+++ importMultipleValues.mjs
@@ -0,0 +1,7 @@
+// importMultipleValues.mjs
+
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+
+console.log(myArray);
+console.log(bulb);
+console.log(multiplyByFour(5));
